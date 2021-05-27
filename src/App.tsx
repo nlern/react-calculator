81974c97cb302f56ee4f148cbddaa31f70602575
Shantanu Dutta <shantanudutta1994@gmail.com>
2021-05-27 13:21:46 +0530

    Updated layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
 import React from 'react';
+import Layout from './components/Layout';
 
 const App: React.FC = () => {
     return (
         <>
-            <header>Sample header</header>
-            <main>main content</main>
-            <footer>
-                <div>
-                    Favicons made by{' '}
-                    <a href="https://www.freepik.com" title="Freepik">
-                        Freepik
-                    </a>{' '}
-                    from{' '}
-                    <a href="https://www.flaticon.com/" title="Flaticon">
-                        www.flaticon.com
-                    </a>
-                </div>
-            </footer>
+            <Layout>hi</Layout>
         </>
     );
 };
