b3bd4412bd7000bb37bcc2f08bd08b466833d7fd
Shantanu Dutta <shantanudutta1994@gmail.com>
2021-05-27 11:28:49 +0530

    1. Added favicons. 2. Updated app title, description. 3. Added tailwindcss.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 const App: React.FC = () => {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <>
+            <header>Sample header</header>
+            <main>main content</main>
+            <footer>
+                <div>
+                    Favicons made by{' '}
+                    <a href="https://www.freepik.com" title="Freepik">
+                        Freepik
+                    </a>{' '}
+                    from{' '}
+                    <a href="https://www.flaticon.com/" title="Flaticon">
+                        www.flaticon.com
+                    </a>
+                </div>
+            </footer>
+        </>
     );
 };
 
